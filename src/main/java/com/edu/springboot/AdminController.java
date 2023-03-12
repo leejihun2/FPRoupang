813a298d535789d6b2d118d7c9081905e6c3c5d9
82104 <82104@LAPTOP-D36E78VQ>
2023-03-12 19:51:51 +0900

    야호~

--- src/main/java/com/edu/springboot/AdminController.java
+++ src/main/java/com/edu/springboot/AdminController.java
@@ -28,16 +28,18 @@ public class AdminController {
 	
 	@RequestMapping("/admin/index.do")
 	public String admin(Principal principal,HttpSession session) {
-		String loginId = principal.getName();
-		String member_idx = member_dao.member_idx(loginId);
-
-		SellRightDTO dto  = member_dao.LoginUser(member_idx);
-		String Authority = dto.getAuthority();
-
-		if(Authority.equals("Seller"))
-		{
-			return "redirect:/productInsert";
-		}
+		
+		//오류나서 주석 처리 했더용 - 지훈
+//		String loginId = principal.getName();
+//		String member_idx = member_dao.member_idx(loginId);
+//
+//		SellRightDTO dto  = member_dao.LoginUser(member_idx);
+//		String Authority = dto.getAuthority();
+//
+//		if(Authority.equals("Seller"))
+//		{
+//			return "redirect:/productInsert";
+//		}
 		
 		return "/admin/index";
 	}
