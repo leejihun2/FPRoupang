dbe26fc80e664667efd9cbb7d8a8d203211d0db8
jeonghoonoh <jeonghoonoh@localhost>
2023-03-15 16:02:20 +0900

    Merge branch 'main' of https://github.com/leejohun/FPRoupang.git into main

--- src/main/java/com/edu/springboot/OrderController.java
+++ src/main/java/com/edu/springboot/OrderController.java
@@ -1,10 +1,15 @@
 package com.edu.springboot;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.edu.springboot.jdbc.GoodsOrderDTO;
 import com.edu.springboot.jdbc.OrderService;
 
 @Controller
@@ -13,8 +18,13 @@ public class OrderController {
 	@Autowired
 	OrderService dao;
 	
-	@RequestMapping(value = "/orderlist.do", method = RequestMethod.GET)
-	public String member1() {
+	@RequestMapping("/orderlist.do")
+	public String orderlist(Model model, GoodsOrderDTO godto) {
+		
+		List<String>orderList= new ArrayList<String>();
+		ArrayList<GoodsOrderDTO>lists = dao.select_goods_order(orderList);
+		
+		
 		return "/orderview/orderList";
 	}
 }
--- src/main/java/com/edu/springboot/jdbc/OrderService.java
+++ src/main/java/com/edu/springboot/jdbc/OrderService.java
@@ -1,8 +1,14 @@
 package com.edu.springboot.jdbc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
 public interface OrderService {
 
+	ArrayList<GoodsOrderDTO> select_goods_order(List<String> orderList);
+
+	
 }
