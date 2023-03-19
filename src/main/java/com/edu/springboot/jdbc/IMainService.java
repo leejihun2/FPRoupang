6a5f15ae5295fca843a69f78b500d452a3160a18
82104 <82104@DESKTOP-J3BO8L7>
2023-03-20 02:55:28 +0900

    메인서비스 DAO수정

--- src/main/java/com/edu/springboot/jdbc/IMainService.java
+++ src/main/java/com/edu/springboot/jdbc/IMainService.java
@@ -7,6 +7,9 @@ import org.apache.ibatis.annotations.Mapper;
 @Mapper
 public interface IMainService {
 	
+	
+	ArrayList<TotalJourneyDTO> adDigital_list();
+	ArrayList<TotalJourneyDTO> adBeauty_list();
 	ArrayList<TotalJourneyDTO> adJourney_list();
 	ArrayList<TotalJourneyDTO> todayShop();
 	ArrayList<TotalJourneyDTO> likeCate();
